5a7dba330666db59ce2cb5a570894e260d2bd7d9
Elvia Benedith <ejbenedith@gmail.com>
2023-02-14 23:36:01 +0100

    API, routes, migrations and seeder

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+
+    public function index()
+    {
+       $quotes = Quote::all();
+       return $quotes;
+    }
+
+    
+    public function store(Request $request)
+    {
+        $quote = new Quote();
+        $quote->author_name = $request->author_name;
+        $quote->quote_text = $request->quote_text;
+        $quote->oustanding = $request->oustanding;
+        $quote->image = $request->image;
+
+        $quote->save();
+    }
+
+    
+    public function show($id)
+    {
+       $quote = Quote::find($id);
+       return $quote;
+    }
+
+    
+    public function update(Request $request, $id)
+    {
+        $quote = Quote::findOrFail($request->id);
+        $quote->author_name = $request->author_name;
+        $quote->quote_text = $request->quote_text;
+        $quote->oustanding = $request->oustanding;
+        $quote->image = $request->image;
+
+        $quote->save();
+        return $quote;
+    }
+
+   
+    public function destroy($id)
+    {
+        $quote = Quote::destroy($id);
+        return $quote;
+    }
+}
