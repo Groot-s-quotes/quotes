2365a42982e2624e8922da348355416649bcdb89
Elvia Benedith <ejbenedith@gmail.com>
2023-02-16 23:14:21 +0100

    modify LOINPAGE name

--- groot-s-quotes-react/src/pages/HomePage/HomePage.jsx
+++ groot-s-quotes-react/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 import './HomePage'
 
 const HomePage = () => {
   return (
     <div className="-page">
+      <Header/>
 
     </div>
   )
