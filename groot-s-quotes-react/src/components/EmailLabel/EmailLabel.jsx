64e251417ca11fd5bde7dcb4f0c429d1cc1206dc
Elvia Benedith <ejbenedith@gmail.com>
2023-02-20 00:04:52 +0100

    merge log-page

--- groot-s-quotes-react/src/components/EmailLabel/EmailLabel.jsx
+++ groot-s-quotes-react/src/components/EmailLabel/EmailLabel.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import './EmailLabel.css';
 
-<<<<<<< HEAD:groot-s-quotes-react/src/components/Email_si_Label/Email_si_Label.jsx
-function Email_si_Label() {
-    return (
-        <>
-        <div className='form-box'>
-            <div class='f5-input'>
-=======
 function EmailLabel({handleInput, loginInput}) {
     console.log(loginInput);
     return (
         <>
         <div className='form-box'>
             <div className='f5-input'>
->>>>>>> log-page:groot-s-quotes-react/src/components/EmailLabel/EmailLabel.jsx
                 <input
                 type='email'
                 className='label-up'
@@ -25,13 +17,10 @@ function EmailLabel({handleInput, loginInput}) {
             /> 
             <label>Email</label>
             </div>
-<<<<<<< HEAD:groot-s-quotes-react/src/components/Email_si_Label/Email_si_Label.jsx
-=======
             <span>{loginInput.error_list.email}</span>
->>>>>>> log-page:groot-s-quotes-react/src/components/EmailLabel/EmailLabel.jsx
         </div>
         </>
       )
     }
 
-export default Email_si_Label
+export default EmailLabel
--- groot-s-quotes-react/src/components/PasswordLabel/PasswordLabel.jsx
+++ groot-s-quotes-react/src/components/PasswordLabel/PasswordLabel.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import './PasswordLabel';
 
-<<<<<<< HEAD:groot-s-quotes-react/src/components/Password_si_Label/Password_si_Label.jsx
-function Password_si_Label() {
-    return (
-        <>
-        <div className='form-box'>
-            <div class='f5-input'>
-=======
 function PasswordLabel({handleInput, loginInput}) {
     return (
         <>
         <div className='form-box'>
             <div className='f5-input'>
->>>>>>> log-page:groot-s-quotes-react/src/components/PasswordLabel/PasswordLabel.jsx
                 <input
                 type='password'
                 className='label-up'  
@@ -24,13 +16,10 @@ function PasswordLabel({handleInput, loginInput}) {
                 /> 
             <label>Password</label>
             </div>
-<<<<<<< HEAD:groot-s-quotes-react/src/components/Password_si_Label/Password_si_Label.jsx
-=======
             <span>{loginInput.error_list.password}</span>
->>>>>>> log-page:groot-s-quotes-react/src/components/PasswordLabel/PasswordLabel.jsx
         </div>
         </>
       )
     }
 
-export default Password_si_Label
+export default PasswordLabel
