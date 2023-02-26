b78921b62f496166534409e790b4753c07016546
Elvia Benedith <ejbenedith@gmail.com>
2023-02-27 00:32:41 +0100

    php unit test UserTest working and QuoteTest file created

---
+++ backEnd/tests/Unit/QuoteTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class QuoteTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ backEnd/tests/Unit/UserTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+
+    //Check if user exists in database
+    public function test_user_duplication()
+    {
+        $user1 = User::make([
+            'name' => 'John Doe',
+            'email' => 'johndoe@gmail.com'
+        ]);
+
+        $user2 = User::make([
+            'name' => 'Mary Jane',
+            'email' => 'maryjane@gmail.com'
+        ]);
+
+        $this->assertTrue($user1->name != $user2->name);
+    }
+
+    public function test_delete_user()
+    {
+        $user = User::factory()->count(1)->make();
+
+        $user = User::first();
+
+        if($user) {
+            $user->delete();
+        }
+
+        $this->assertTrue(true);
+    }
+        //Perform a post() request to add a new user
+        public function test_if_it_stores_new_users()
+        {
+            $response = $this->post('/register', [
+                'name' => 'Maria',
+                'email' => 'maria@gmail.com',
+                'password' => 'maria1234',
+                'password_confirmation' => 'maria1234'
+            ]);
+    
+            $response->assertRedirect('/home');
+        }
+
+        public function test_if_data_exists_in_database()
+        {
+            $this->assertDatabaseHas('users', [
+                'name' => 'Maria'
+            ]);
+        }
+
+        public function test_if_data_does_not_exists_in_database()
+        {
+            $this->assertDatabaseMissing('users', [
+                'name' => 'Yassmina'
+            ]);
+        }
+
+}
