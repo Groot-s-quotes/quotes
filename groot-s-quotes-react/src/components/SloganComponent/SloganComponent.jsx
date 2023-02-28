fcc905a5b319b22d5f62c33f1394862540db8490
JasMarin <yasminamarin17@gmail.com>
2023-02-28 09:32:01 +0100

    welcome text

--- groot-s-quotes-react/src/components/SloganComponent/SloganComponent.jsx
+++ groot-s-quotes-react/src/components/SloganComponent/SloganComponent.jsx
@@ -5,7 +5,8 @@ const SloganComponent = () => {
   return (
     <div className='intro-container'>
       <h1>Groot's Quotes</h1>
-      <p>At groot's qotes we inspire your inspiration, with quotes from great authors, great anonymous quotes and not least your own quotes to inspire other users. Register and be part of the groot community, create your own dashboart with the phrases you like the most.</p>
+      <p>At Groot's Quotes we inspire your inspiration with quotes from great authors, exceptional anonymous quotes and, not least, your own quotes tomotivate other users. 
+      Register and be part of the Groot community. Create your own dashboart with the phrases you like the most.</p>
     </div>
   )
 }
